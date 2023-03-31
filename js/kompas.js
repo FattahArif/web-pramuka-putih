8ad3b09b11cf4aab9a2dc01266ad6a596adc7376
FattahArif <fattaharif87@gmail.com>
2023-03-31 23:13:43 +0700

    update-event

--- js/kompas.js
+++ js/kompas.js
@@ -12,3 +12,42 @@ window.onscroll = function () {
     
       scrollAwal = scrollAkhir
 }
+//  Slider
+
+// Get carousel element
+var carousel = document.querySelector('.carousel');
+
+// Get carousel items
+var carouselItems = carousel.querySelectorAll('.carousel-item');
+
+// Get carousel controls
+var carouselControlPrev = carousel.querySelector('.carousel-control-prev');
+var carouselControlNext = carousel.querySelector('.carousel-control-next');
+
+// Set initial active item
+var activeItemIndex = 0;
+carouselItems[activeItemIndex].classList.add('active');
+
+// Add event listener to prev control
+carouselControlPrev.addEventListener('click', function(e) {
+  e.preventDefault();
+  carouselItems[activeItemIndex].classList.remove('active');
+  activeItemIndex--;
+  if (activeItemIndex < 0) {
+    activeItemIndex = carouselItems.length - 1;
+  }
+  carouselItems[activeItemIndex].classList.add('active');
+});
+
+// Add event listener to next control
+carouselControlNext.addEventListener('click', function(e) {
+  e.preventDefault();
+  carouselItems[activeItemIndex].classList.remove('active');
+  activeItemIndex++;
+  if (activeItemIndex >= carouselItems.length) {
+    activeItemIndex = 0;
+  }
+  carouselItems[activeItemIndex].classList.add('active');
+});
+
+
