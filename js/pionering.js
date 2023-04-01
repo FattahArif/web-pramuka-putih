cf1816d4005856ee98c413c50c74b29f46cbb12a
FattahArif <fattaharif87@gmail.com>
2023-04-01 07:56:32 +0700

    update-terbaruu

---
+++ js/pionering.js
@@ -0,0 +1,20 @@
+var slideIndex = 0;
+var slides = document.getElementsByClassName("slider-slide");
+
+function showSlides() {
+  for (var i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {
+    slideIndex = 1;
+  }
+  slides[slideIndex-1].style.display = "block";
+  setTimeout(showSlides, 3000); // Ubah angka 3000 dengan interval waktu yang diinginkan (dalam milidetik)
+}
+
+showSlides();
+
+setInterval(() => {
+  showSlides();
+}, 3000);
